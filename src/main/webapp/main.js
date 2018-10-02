e6f982549859bbc6db3bf76b5854a19c135b197a
Luopm <luo.pingming@ztesoft.com>
2018-10-02 08:57:41 +0800

    添加CSS

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -6,27 +6,8 @@ require.config({
     }
 })
 
-require(['lib/plugin/output.js','pbook/views/homeView','css!main.css'],
-    function (output,homeView) {
+require(['pbook/views/homeView','css!main.css'],
+    function (homeView) {
     homeView.render("#app");
-    $(".content").append(
-        '<div class="input-group">' +
-        '    <input type="text" class="form-control" id="input" placeholder="Recipient\'s username" aria-describedby="basic-addon2">' +
-        '    <span class="input-group-addon" id="basic-addon2">OK</span>' +
-        '</div>' );
-    $("#basic-addon2").click(function () {
-        $.ajax({
-            method:'post',
-            url:'../user/all',
-            async:true,
-            data:{},
-            dataType:'json',
-            success:function (result) {
-                output.output(JSON.stringify(result.list),150,'.content');
-            }
-        })
-
-        //var str = $("#input").val();
-
-    })
+        // homeView.render("#app");
 });
--- src/main/webapp/pbook/views/homeView.js
+++ src/main/webapp/pbook/views/homeView.js
@@ -1,8 +1,31 @@
-define(['text!../templates/home.html'],function (tem) {
+define(['text!../templates/home.html','lib/plugin/output.js','css!../style/homeStyle.css'],
+    function (tem,output) {
     var view ={
         render : function (el) {
-            $(el).html(tem);
+            $(el).append(tem);
+            this.init();
+        },
+        init : function () {
+            var that = this;
+            $("#home .modal-body").append(
+                '<div class="input-group">' +
+                '    <input type="text" class="form-control" id="input" placeholder="Recipient\'s username" aria-describedby="basic-addon2">' +
+                '    <span class="input-group-addon" id="basic-addon2">OK</span>' +
+                '</div>' );
+            $("#basic-addon2").click(function () {
+                $.ajax({
+                    method:'post',
+                    url:'../user/all',
+                    async:true,
+                    data:{},
+                    dataType:'json',
+                    success:function (result) {
+                        output.output(JSON.stringify(result.list),150,'.input-group');
+                    }
+                })
+            })
         }
     }
+
     return view;
 });
